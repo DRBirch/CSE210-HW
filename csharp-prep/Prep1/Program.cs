15719a4cbc90d8aa05da7c175ddc9c806edb38c4
Dallen Birchenough <bir20033@byui.edu>
2026-04-25 14:53:38 -0600

    James Bond intro

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,15 @@ using System;
 
 class Program
 {
+    //Ask user for name and display like James BOnd intro
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }

14bbedfbe957c4567b6990973fe7b9dfd86caae4
Dallen Birchenough <bir20033@byui.edu>
2026-04-25 16:11:13 -0600

    Core 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string correct = "False";
+        Console.Write("What is the magic number? ");
+        string numberInput = Console.ReadLine();
+        int number = int.Parse(numberInput);
+
+        while (correct == "False")
+        {
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            int guess = int.Parse(guessInput);
+            if (guess > number)
+                {
+                Console.WriteLine("Lower");
+                }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                correct = "True";
+            }
+        }
     }
 }

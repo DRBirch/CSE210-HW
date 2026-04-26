910ac83d3ba5ad854727a6c9ab45d3819bc6c573
Dallen Birchenough <bir20033@byui.edu>
2026-04-25 20:32:28 -0600

    Core 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input = 1;
+        int sum = 0;
+
+        while (input != 0)
+        {
+            Console.Write("Enter a number: ");
+            string inputNumber = Console.ReadLine();
+            input = int.Parse(inputNumber);
+            numbers.Add(input);
+        }
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+            sum = sum + numbers[i];
+        }
+        Console.WriteLine($"The sum is: {sum}");
     }
 }

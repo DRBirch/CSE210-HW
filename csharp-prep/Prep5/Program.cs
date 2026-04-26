607eaf71dcf4a24887d36675a37aea7718474b22
Dallen Birchenough <bir20033@byui.edu>
2026-04-25 20:59:49 -0600

    Core

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displayMessage();
+
+        string userName = askName();
+        int userNumber = askNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        int birthYear;
+        askBirthYear(out birthYear);
+        Result(userName, squaredNumber, birthYear);
+    }
+
+    static void displayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string askName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int askNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    
+    static void askBirthYear(out int birthYear)
+    {
+        Console.Write($"Enter the year you were born: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void Result(string name, int square, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine($"{name}, you will turn {2026 - birthYear} years old this year.");
     }
 }

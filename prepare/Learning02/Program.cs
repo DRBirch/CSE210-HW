7eda261a69ac17edbc208406c2ca940ef36c3c7a
Dallen Birchenough <bir20033@byui.edu>
2026-04-29 17:23:51 -0600

    Realized it was all on one file so separated the code, Step 8

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -11,35 +11,21 @@ class Program
         programmer._jobTitle = "Software Engineer";
         programmer._startYear = 2019;
         programmer._endYear = 2020;
-        programmer.DisplayInformation();
+
 
         Job manager = new Job();
         manager._company = "Apple";
         manager._jobTitle = "Manager";
         manager._startYear = 2022;
         manager._endYear = 2023;
-        manager.DisplayInformation();
+
 
         Resume myResume = new Resume();
         myResume._jobs.Add(programmer);
         myResume._jobs.Add(manager);
-        myResume._jobs[0]._jobTitle
-    }
-    public class Job
-    {
-        public string _company;
-        public string _jobTitle;
-        public int _startYear;        
-        public int _endYear;
+        myResume._name = "Allsion Rose";
 
-        public void DisplayInformation()
-        {
-            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-        }
-    }    
-    public class Resume
-    {
-        public string _name;
-        public List<Job> _jobs = new List<Job>();
-    }
+        myResume.DisplayResume();
+
+    } 
 }

b30130a951ae59616416eb70bd103450d21483c1
Dallen Birchenough <bir20033@byui.edu>
2026-04-29 16:40:57 -0600

    Steps 2-4 and part of 5

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+    {        
+        Job programmer = new Job();        
+        programmer._company = "Microsoft";
+        programmer._jobTitle = "Software Engineer";
+        programmer.DisplayInformation();
     }
+    public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        //public int _startYear;        
+        //public int _endYear;
+
+        public void DisplayInformation()
+        {
+            Console.WriteLine($"Company: {_company} \nJob Title: {_jobTitle}");
+        }
+    }    
 }

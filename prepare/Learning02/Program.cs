a31dba46331a9c4427b18741b40cc66f82ad4acf
Dallen Birchenough <bir20033@byui.edu>
2026-04-29 17:02:45 -0600

    Parts 5, 6, and 7

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,18 +9,37 @@ class Program
         Job programmer = new Job();        
         programmer._company = "Microsoft";
         programmer._jobTitle = "Software Engineer";
+        programmer._startYear = 2019;
+        programmer._endYear = 2020;
         programmer.DisplayInformation();
+
+        Job manager = new Job();
+        manager._company = "Apple";
+        manager._jobTitle = "Manager";
+        manager._startYear = 2022;
+        manager._endYear = 2023;
+        manager.DisplayInformation();
+
+        Resume myResume = new Resume();
+        myResume._jobs.Add(programmer);
+        myResume._jobs.Add(manager);
+        myResume._jobs[0]._jobTitle
     }
     public class Job
     {
         public string _company;
         public string _jobTitle;
-        //public int _startYear;        
-        //public int _endYear;
+        public int _startYear;        
+        public int _endYear;
 
         public void DisplayInformation()
         {
-            Console.WriteLine($"Company: {_company} \nJob Title: {_jobTitle}");
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
         }
     }    
+    public class Resume
+    {
+        public string _name;
+        public List<Job> _jobs = new List<Job>();
+    }
 }
